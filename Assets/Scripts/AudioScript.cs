5ad9d8060a005be1b4114de7a66697e2c093bb66
Kenneth McLachlan <121731402+KennethMcLachlan@users.noreply.github.com>
2023-11-30 15:31:52 -0800

    Finished Virtual Piano

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioScript : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip _audioClip;
+
+    [SerializeField]
+    private AudioSource _audioSource;
+
+    [SerializeField]
+    private float _pitch;
+
+    public void PlayKey()
+    {
+        _audioSource.pitch = _pitch;
+        _audioSource.PlayOneShot(_audioClip);
+    }
+}
---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ButtonPressed()
+    {
+        Debug.Log("This button is pressed");
+    }
+}
